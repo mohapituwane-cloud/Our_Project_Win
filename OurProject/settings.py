c80ec5604ce5d0a01509f0c12e7e68a7798d2c70
mohapituwane-cloud <mohapituwane@icloud.com>
2026-09-22 18:45:37 +0200

    fixing css template

--- OurProject/settings.py
+++ OurProject/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
